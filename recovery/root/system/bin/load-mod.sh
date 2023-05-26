72d6156648e3360de15ba52195ab0d46b4cf7b7d
niels <nielsraddatz@gmx.de>
2023-05-26 23:50:05 +0200

    oneplus_lemonadep: update fix modem not flashable in fastbootd
    Change-Id: I2b8f851a7940baca9a46282d6c09239b08715173

--- recovery/root/system/bin/load-mod.sh
+++ recovery/root/system/bin/load-mod.sh
@@ -1,13 +1,8 @@
 #!/system/bin/sh
 
-if /system/bin/grep -Fq twrpfastboot /proc/cmdline
-then
-    /system/bin/mount --bind /vendor/lib /lib
-    #/system/bin/modprobe -d /lib/modules msm_drm dsi_display0=qcom,mdss_dsi_samsung_amb670yf01_dsc_cmd:
-    /system/bin/modprobe -d /lib/modules msm_drm #`/system/bin/cat /proc/cmdline | /system/bin/grep -o -e "msm_drm.*: " | /system/bin/cut -c 9-`
-    /system/bin/umount /lib
-else
-    /system/bin/modprobe -d /lib/modules msm_drm
+if /system/bin/grep -Fq twrpfastboot /proc/cmdline; then
+    /system/bin/modprobe -d /vendor/lib/modules/1.1 msm_drm
+    /system/bin/modprobe -d /vendor/lib/modules msm_drm
 fi
 
 
